2e54596e143e1df6f46c03f456eb774884814526
The-Leo <107513599+The-Leo@users.noreply.github.com>
2022-09-03 16:54:48 +0000

    first commit

---
+++ flaskr/__init__.py
@@ -0,0 +1,19 @@
+# Import your dependencies
+from flask import Flask, jsonify
+# Define the create_app function
+def create_app(test_config=None):
+ # Create and configure the app
+ # Include the first parameter: Here, __name__is the name of the current Python module.
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello_world():
+        return jsonify({'message':'HELLO WORLD'})
+        
+    @app.route('/Smiley')
+    def smiley():
+        return jsonify({'message':':)'})
+
+        # return 'Hello, World!'
+ # Return the app instance
+    return app
